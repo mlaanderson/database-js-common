782b720b7ca0d21bef64061eb86f719c533e4f82
Andrew Barnett <andrew.barnett@franklinsquare.com>
2018-10-23 13:46:05 -0400

    feat(multilevel-config): support multilevel config
    Adds support for multilevel objects and arrays to the query string
    parsing. For example, the string
    `ssl[ca]=ca.pem&ssl[key]=key.pem&ssl[cert]=cert.pem` would parse to:
    
    ```
    {
      ssl: {
        ca: 'ca.pem',
        key: 'key.pem',
        cert: 'cert.pem'
      }
    }
    ```

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var keyBreaker = /([^\[\]]+)|(\[\])/g,
+    digitTest = /^\d+$/;
+
 function parseConnectionParams(paramstring, parseValues = false) {
     const params = {};
     const numberRegex = /^[-+]?(\d+\.)?\d+(E[-+]?\d+)?$/i;
@@ -13,9 +16,25 @@ function parseConnectionParams(paramstring, parseValues = false) {
             } :
             (value) => value;
 
+        // based on $.String.deparam: https://github.com/jupiterjs/jquerymx/blob/master/lang/string/deparam/deparam.js
         paramstring.split('&').map(s => {
             let [key, val] = s.split('=');
-            params[key] = valuesPreprocessor(val);
+            let current = params;
+            let parts = key.match(keyBreaker);
+            for(var i = 0; i < parts.length - 1; i++) {
+                let part = parts[i];
+                if(!current[part]) {
+                    // if what we are pointing to looks like an array
+                    current[part] = digitTest.test(parts[i+1]) || parts[i+1] == "[]" ? [] : {}
+                }
+                current = current[part];
+            }
+            let lastPart = parts[parts.length - 1];
+            if(lastPart == "[]"){
+                current.push(valuesPreprocessor(val));
+            } else {
+                current[lastPart] = valuesPreprocessor(val);
+            }
         });
     }
 
